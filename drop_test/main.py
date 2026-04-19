a7981651acc3bdc5b263f40954d354068b03d316
Bor Bence <132979209+bebe11111@users.noreply.github.com>
2026-04-19 17:33:33 +0200

    drop_test

--- drop_test/main.py
+++ drop_test/main.py
@@ -1,9 +1,24 @@
 from pathlib import Path
 
+def min_num_of_drops(N, H):
+    dp = [0] * (N + 1)
+    m = 0
+    
+    while dp[N] < H:
+        m += 1
+        for i in range(N, 0, -1):
+            dp[i] = dp[i] + dp[i - 1] + 1
+    
+    return m
 
 def main():
-    data = Path("input.txt").read_text(encoding="utf-8")
-    print(data, end="")
+    data = Path("drop_test/input.txt").read_text(encoding="utf-8")
+    lines = data.splitlines()
+    for line in lines:
+        N, H = map(int, line.replace(" ", "").split(","))
+        
+        result = min_num_of_drops(N, H)
+        print(result)
 
 
 if __name__ == "__main__":
